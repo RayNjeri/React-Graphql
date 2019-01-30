2f0aa7f3593c9e0845fa2f968ad851c5da666dec
Rachael Njeri <rachael.njeri@andela.com>
2019-01-30 15:11:37 +0300

    Bug fix
    Signed-off-by: Rachael Njeri <rachael.njeri@andela.com>

--- schema/schema.js
+++ schema/schema.js
@@ -19,7 +19,7 @@ const CompanyType = new GraphQLObjectType({
       type: new GraphQLList(UserType),
       resolve(parentValue, args) {
         return axios
-          .get(`http://localhost:3000/companies/${parentValue.companyId}/users`)
+          .get(`http://localhost:3000/companies/${parentValue.id}/users`)
           .then(resp => resp.data);
       }
     }
