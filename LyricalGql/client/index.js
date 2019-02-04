08c87b16b9da156f431103b1cbf2e50a396f2a36
Rachael Njeri <rachael.njeri@andela.com>
2019-02-04 09:58:39 +0300

    Implement react-router
    Signed-off-by: Rachael Njeri <rachael.njeri@andela.com>

--- LyricalGql/client/index.js
+++ LyricalGql/client/index.js
@@ -2,15 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
+import { Route, hashHistory, IndexRoute, Router } from 'react-router';
 
 import SongList from './components/SongList';
+import App from './components/App';
 
 const client = new ApolloClient({});
 
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <SongList />
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={SongList} />
+        </Route>
+      </Router>
     </ApolloProvider>
   );
 };
