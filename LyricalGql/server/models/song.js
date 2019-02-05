2422586cd11e559ea6e1e547cd922233cfbf0301
Rachael Njeri <rachael.njeri@andela.com>
2019-02-05 11:54:14 +0300

    Mongo error $pushAll bug fix
    Signed-off-by: Rachael Njeri <rachael.njeri@andela.com>

--- LyricalGql/server/models/song.js
+++ LyricalGql/server/models/song.js
@@ -1,26 +1,21 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const SongSchema = new Schema({
-  title: { type: String },
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: 'user'
+const SongSchema = new Schema(
+  {
+    title: { type: String },
+    user: { type: Schema.Types.ObjectId, ref: 'user' },
+    lyrics: [{ type: Schema.Types.ObjectId, ref: 'lyric' }]
   },
-  lyrics: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'lyric'
-    }
-  ]
-});
+  { usePushEach: true }
+);
 
 SongSchema.statics.addLyric = function(id, content) {
   const Lyric = mongoose.model('lyric');
 
   return this.findById(id).then(song => {
     const lyric = new Lyric({ content, song });
-    song.lyrics.push(lyric);
+    song.lyrics.push(lyric.id);
     return Promise.all([lyric.save(), song.save()]).then(
       ([lyric, song]) => song
     );
