c4d40407639c2a13a74e52cad8c6c389feaea9e0
Rachael Njeri <rachael.njeri@andela.com>
2019-02-06 12:11:27 +0300

    Automatice component rerenders
    Signed-off-by: Rachael Njeri <rachael.njeri@andela.com>

--- Auth/client/components/Header.js
+++ Auth/client/components/Header.js
@@ -3,15 +3,26 @@ import { graphql } from 'react-apollo';
 import { Link } from 'react-router';
 
 import query from '../queries/CurrentUser';
+import mutation from '../mutations/Logout';
 
 class Header extends Component {
+  onLogoutClick() {
+    this.props.mutate({
+      refetchQueries: [{ query }]
+    });
+  }
+
   renderButtons() {
     const { loading, user } = this.props.data;
     if (loading) {
       return <div />;
     }
     if (user) {
-      return <div>Logout</div>;
+      return (
+        <li>
+          <a onClick={this.onLogoutClick.bind(this)}>Logout</a>
+        </li>
+      );
     } else {
       return (
         <div>
@@ -39,4 +50,4 @@ class Header extends Component {
   }
 }
 
-export default graphql(query)(Header);
+export default graphql(mutation)(graphql(query)(Header));
