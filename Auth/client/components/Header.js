817ec2f07bde8e33b04b3b28b0afaf1e8d7a3c9d
Rachael Njeri <rachael.njeri@andela.com>
2019-02-06 11:56:48 +0300

    Impelement the login and logout buttons
    Signed-off-by: Rachael Njeri <rachael.njeri@andela.com>

--- Auth/client/components/Header.js
+++ Auth/client/components/Header.js
@@ -1,12 +1,41 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
 
 import query from '../queries/CurrentUser';
 
 class Header extends Component {
+  renderButtons() {
+    const { loading, user } = this.props.data;
+    if (loading) {
+      return <div />;
+    }
+    if (user) {
+      return <div>Logout</div>;
+    } else {
+      return (
+        <div>
+          <li>
+            <Link to="/signup">Sign Up</Link>
+          </li>
+          <li>
+            <Link to="/login"> Login</Link>
+          </li>
+        </div>
+      );
+    }
+  }
   render() {
-    console.log('ggggg', this.props.data);
-    return <div>Header</div>;
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <Link to="/" className="brand-logo left">
+            Home
+          </Link>
+          <ul className="right">{this.renderButtons()}</ul>
+        </div>
+      </nav>
+    );
   }
 }
 
