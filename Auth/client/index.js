cd68e25c84cc3fa747827be1def994e3e03e47bd
Rachael Njeri <rachael.njeri@andela.com>
2019-02-06 10:50:00 +0300

    Client side set up
    Signed-off-by: Rachael Njeri <rachael.njeri@andela.com>

--- Auth/client/index.js
+++ Auth/client/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, IndexRoute, Route, hashHistory } from 'react-router';
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+
+import App from './components/App';
+
+const client = new ApolloClient({
+  dataIdFromObject: o => o.id
+});
 
 const Root = () => {
-  return <div>Auth Starter</div>;
+  return (
+    <ApolloProvider client={client}>
+      <Router history={hashHistory}>
+        <Route path="/" component={App} />
+      </Router>
+    </ApolloProvider>
+  );
 };
 
 ReactDOM.render(<Root />, document.querySelector('#root'));
