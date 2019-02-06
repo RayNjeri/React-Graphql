6adb5f74c0182774767d44a47100648d311eb581
Rachael Njeri <rachael.njeri@andela.com>
2019-02-06 11:02:43 +0300

    Including cookies in graphql requests
    Signed-off-by: Rachael Njeri <rachael.njeri@andela.com>

--- Auth/client/components/Header.js
+++ Auth/client/components/Header.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+
+import query from '../queries/CurrentUser';
 
 class Header extends Component {
   render() {
+    console.log('ggggg', this.props.data);
     return <div>Header</div>;
   }
 }
 
-export default Header;
+export default graphql(query)(Header);
--- Auth/client/index.js
+++ Auth/client/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, IndexRoute, Route, hashHistory } from 'react-router';
-import ApolloClient from 'apollo-client';
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 
 import App from './components/App';
 
+const networkInterface = createNetworkInterface({
+  uri: '/graphql',
+  opts: {
+    credentials: 'same-origin'
+  }
+});
+
 const client = new ApolloClient({
+  networkInterface,
   dataIdFromObject: o => o.id
 });
 
