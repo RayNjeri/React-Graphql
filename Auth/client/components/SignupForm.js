2a260207d2b79a23af9404e09f3c3af6b5f5a92c
Rachael Njeri <rachael.njeri@andela.com>
2019-02-07 09:47:59 +0300

    Sign up error handling
    Signed-off-by: Rachael Njeri <rachael.njeri@andela.com>

--- Auth/client/components/SignupForm.js
+++ Auth/client/components/SignupForm.js
@@ -5,11 +5,32 @@ import AuthForm from './AuthForm';
 import mutation from '../mutations/Signup';
 
 class SignupForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      errors: []
+    };
+  }
+
+  onSubmit({ email, password }) {
+    this.props
+      .mutate({
+        variables: { email, password }
+      })
+      .catch(res => {
+        const errors = res.graphQLErrors.map(error => error.message);
+        this.setState({ errors });
+      });
+  }
+
   render() {
     return (
       <div>
-        <h3>AuthForm</h3>
-        <AuthForm />
+        <h3>Sign up</h3>
+        <AuthForm
+          errors={this.state.errors}
+          onSubmit={this.onSubmit.bind(this)}
+        />
       </div>
     );
   }
